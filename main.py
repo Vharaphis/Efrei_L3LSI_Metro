1814f930e79244d33a50636d51b0cc14693ef342
Vharaphis <55180094+Vharaphis@users.noreply.github.com>
2022-10-30 00:26:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ import Drawing
 
 #create dico with the file
 f = open("data/metro.txt", "r")
-dico = data_structure.init_map(f)
+dico = DataStructure.init_map(f)
 #dico = (data_structure.init_station(f))
 #print("There is a dico :",dico) #print all the dico data structure
 
 #call kruskal with the dico of dico
 def display_acpm():
-    ACPM = kruskal_acpm.kruskal(dico)
+    ACPM = KruskalAcpm.kruskal(dico)
     print('List of egdes from the ACPM of Paris metro map :', ACPM)  #print all the ACPM
 
 #call kruskal with the dico of dico
@@ -22,4 +22,4 @@ def display_shortest_path(departure, destination):
     ...
 
 #display the interface
-display = interface
+display = Interface
